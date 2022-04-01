40d1476d41e774e15eda5765dffc6ae5c14d0f7b
Başak Tüysüz <85128377+basaktuysuz@users.noreply.github.com>
2022-04-01 22:52:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#   Question1
+name = input("Please enter your name: \n")
+print("Hello " + name + ".")
+stdID = input ("What is your Student ID? \n")
+print("Your ID is " + stdID + ".")
+
+#   Question2
+number1 = input("Please enter num 1: ")
+number2 = input("Please enter num 2: ")
+summation = int(number1) + int(number2)
+product = int(number1) * int(number2)
+difference = int(number1) - int(number2)
+print("Number 1 is : " + number1)
+print("Number 2 is : " + number2)
+print("Summation  : " + summation)
+print("Difference  : " + difference)
+print("Product : " + product)
+
+#   Question3
+print("*\n**\n****")
